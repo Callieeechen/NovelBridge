2d271ca97fa2b988bd05d52861729861d927b70e
Aryan Gupta <emperorary@Aryans-MacBook-Air-2.local>
2024-03-17 19:19:01 +0800

    addine firebase to reading.vue

---
+++ NovelBridge/src/components/Reading.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+
+import firebaseApp from '../firebase.js';
+import { getFirestore } from 'firebase/firestore';
+import { collection, getDocs, doc , deleteDoc } from 'firebase/firestore';
+const db = getFirestore(firebaseApp);
+let docs = await getDocs(collection(db, 'Books'))
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
