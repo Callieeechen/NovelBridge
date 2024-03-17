82e1c6df58eb7de330a9578fd99a1b4629fb3c1a
Aryan Gupta <emperorary@Aryans-MacBook-Air-2.local>
2024-03-17 20:23:55 +0800

    Added script: data & next/prev chapter functions

--- NovelBridge/src/components/Reading.vue
+++ NovelBridge/src/components/Reading.vue
@@ -7,13 +7,44 @@
 import firebaseApp from '../firebase.js';
 import { getFirestore } from 'firebase/firestore';
 import { collection, getDocs, doc , deleteDoc } from 'firebase/firestore';
-const db = getFirestore(firebaseApp);
-let docs = await getDocs(collection(db, 'Books'))
+
+
 
 export default {
+    props: ['bookName'],
+    created() {
+        async function getChapters(bookN) {
+            const db = getFirestore(firebaseApp);
+            const book = await getDocs(collection(db, 'Books',bookN))
+            let chapters = book.data().Chapters 
+            return chapters
+
+        }
+    },
+    mounted() {
+        function goToNextChapter(){
+            for (let i=0;i<len(keys)-1;i++){
+                if chapter == keys[i] {
+                    chapter = keys[i+1]
+                    chapter_data = all_chapters[chapter]
+                }
+            }
+        }
+        function goToPreviousChapter(){
+            for (let i=1;i<len(keys);i++){
+                if chapter == keys[i] {
+                    chapter = keys[i-1]
+                    chapter_data = all_chapters[chapter]
+                }
+            }
+        }
+    }
     data() {
         return {
-
+            all_chapters = getChapters(bookName)
+            keys = all_chapters.getKeys()
+            chapter = keys[0]
+            chapter_data = all_chapters[chapter]
         }
     }
 }
